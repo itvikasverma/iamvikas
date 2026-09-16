c146fe59ed612b3b6c67d5467197f8b8cba8dfb3
itvikasverma <vermavikas0880@gmail.com>
2026-09-16 14:48:51 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/iamvikas",
+  images: {
+    unoptimized: true,
+  },
   allowedDevOrigins: ['192.168.1.14'],
 };
 
